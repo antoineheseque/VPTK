d68bf340850c9633593bc70eb8b9ab05203939a0
Antoine HESEQUE <antoine.heseque@isen.yncrea.fr>
2020-10-14 15:59:18 +0200

    Camera system + comments on scripts

---
+++ Assets/VPTK/Scripts/Editor/LoadMRCameraEditor.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+/// <summary>
+/// Show a dropdown with all the possible cameras in the LoadMRCamera script hierarchy.
+/// </summary>
+[CustomEditor(typeof(LoadMRCamera))]
+public class LoadMRCameraEditor : Editor
+{
+    private int selected = 0;
+    private readonly List<string> options = new List<string>();
+
+    private SerializedProperty selectedWebcam;
+    
+    private void OnEnable()
+    {
+        options.Clear();
+        foreach (var cam in WebCamTexture.devices)
+        {
+            options.Add(cam.name);
+        }
+
+        selectedWebcam = serializedObject.FindProperty("selectedWebcam");
+        selected = options.FindIndex(a => a == selectedWebcam.stringValue);
+    }
+
+    public override void OnInspectorGUI()
+    {
+        selected = EditorGUILayout.Popup("WEBCAM", selected, options.ToArray());
+        selectedWebcam.stringValue = options[selected];
+        
+        serializedObject.ApplyModifiedProperties();
+    }
+}
--- Assets/VPTK/Scripts/LoadMRCamera.cs
+++ Assets/VPTK/Scripts/LoadMRCamera.cs
@@ -1,47 +1,45 @@
-﻿using System;
-using UnityEditor;
+﻿using System.Linq;
 using UnityEngine;
 
 public class LoadMRCamera : MonoBehaviour
 {
-    [SerializeField] private string cameraName;
-    
-    private WebCamDevice selectedWebcam;
+    public string selectedWebcam;
     private static readonly int MainTexture = Shader.PropertyToID("_BaseColorMap");
 
     private void Start()
     {
+        // Show all the cameras in console
         Debug.Log($"[Camera Selector] Number of camera(s): {WebCamTexture.devices.Length}");
         foreach (var cam in WebCamTexture.devices)
-        {
             Debug.Log($"[Camera Selector] {cam.name}");
-            
-            if (cam.name == cameraName)
-                selectedWebcam = cam;
-        }
 
+        // Make sure the camera we selected still exists
+        if (WebCamTexture.devices.All(a => a.name != selectedWebcam))
+        {
+            Debug.LogError($"[Camera Selector] {selectedWebcam} not found.");
+        }
+        
+        // Try init the camera and assign it to a texture. It may also exist but used by another software.
         if (InitCamera())
-            Debug.Log($"[Camera Selector] {selectedWebcam.name} successfully started.");
+            Debug.Log($"[Camera Selector] {selectedWebcam} successfully started.");
         else
-            Debug.LogError($"[Camera Selector] {selectedWebcam.name} can't be started. Is this camera already used?");
+            Debug.LogError($"[Camera Selector] {selectedWebcam} can't be started. Is this camera already used?");
     }
 
+    /// <summary>
+    /// Init the camera
+    /// </summary>
+    /// <returns>true if camera is successfully set in a texture, else otherwise</returns>
     private bool InitCamera()
     {
-        if (string.IsNullOrEmpty(selectedWebcam.name))
+        if (string.IsNullOrEmpty(selectedWebcam))
             return false;
-        
-        WebCamTexture webcamTexture = new WebCamTexture(selectedWebcam.name);
+
+        WebCamTexture webcamTexture = new WebCamTexture(selectedWebcam);
         GetComponent<Renderer>().material.SetTexture(MainTexture, webcamTexture);
 
-        try
-        {
-            webcamTexture.Play();
-        }
-        catch
-        {
-            return false;
-        }
+        try { webcamTexture.Play(); }
+        catch { return false; }
 
         return true;
     }
