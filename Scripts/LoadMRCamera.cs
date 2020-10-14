4c46437987ceb7f105b7d91569d4ef1a81320c54
Antoine HESEQUE <antoine.heseque@isen.yncrea.fr>
2020-10-14 14:34:10 +0200

    First commit

---
+++ Scripts/LoadMRCamera.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using UnityEditor;
+using UnityEngine;
+
+public class LoadMRCamera : MonoBehaviour
+{
+    [SerializeField] private string cameraName;
+    
+    private WebCamDevice selectedWebcam;
+    private static readonly int MainTexture = Shader.PropertyToID("_BaseColorMap");
+
+    private void Start()
+    {
+        Debug.Log($"[Camera Selector] Number of camera(s): {WebCamTexture.devices.Length}");
+        foreach (var cam in WebCamTexture.devices)
+        {
+            Debug.Log($"[Camera Selector] {cam.name}");
+            
+            if (cam.name == cameraName)
+                selectedWebcam = cam;
+        }
+
+        if (InitCamera())
+            Debug.Log($"[Camera Selector] {selectedWebcam.name} successfully started.");
+        else
+            Debug.LogError($"[Camera Selector] {selectedWebcam.name} can't be started. Is this camera already used?");
+    }
+
+    private bool InitCamera()
+    {
+        if (string.IsNullOrEmpty(selectedWebcam.name))
+            return false;
+        
+        WebCamTexture webcamTexture = new WebCamTexture(selectedWebcam.name);
+        GetComponent<Renderer>().material.SetTexture(MainTexture, webcamTexture);
+
+        try
+        {
+            webcamTexture.Play();
+        }
+        catch
+        {
+            return false;
+        }
+
+        return true;
+    }
+}
